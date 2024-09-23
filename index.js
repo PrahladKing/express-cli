6ddb1b4cad37a07fbf334b9d8dda0417d9a16bc0
Prahlad9 <prahlad@myverkoper.com>
2024-09-23 14:14:51 +0530

    made index.js can be run from cli

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-// index.js
-function hello(name) {
-    return `Hello, ${name}!`;
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+const className = process.argv[2];
+
+if (!className) {
+  console.error('Please provide a class name.');
+  process.exit(1);
 }
 
-module.exports = { hello };
+const classDir = path.join(__dirname, 'classes', className);
+const classFilePath = path.join(classDir, `${className}.js`);
+
+const classTemplate = `
+class ${className} {
+  constructor() {
+    // constructor logic
+  }
+  
+  // methods
+}
+
+module.exports = ${className};
+`;
+
+fs.mkdirSync(classDir, { recursive: true });
+fs.writeFileSync(classFilePath, classTemplate.trim());
+console.log(`Class ${className} created successfully at ${classFilePath}`);
